1705a688268e7714614e7c86902dd81159a70893
Cole <cperkins577@gmail.com>
2024-01-10 09:06:20 -0500

    Added attack animation

--- scenes/samurai.gd
+++ scenes/samurai.gd
@@ -4,6 +4,7 @@ extends CharacterBody2D
 @onready var state_machine = animation_tree["parameters/playback"] 
 const SPEED = 150.0
 const JUMP_VELOCITY = -200.0
+var is_attacking = false
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
@@ -17,17 +18,34 @@ func _physics_process(delta):
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-
+	
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction = Input.get_axis("move_left", "move_right")
-	if direction:
+	if direction and is_attacking == false:
 		velocity.x = direction * SPEED
-		if velocity.x <= 0 :
-			$Sprite2D["flip_h"] = true
+		state_machine.travel("walk")
+		if velocity.x <= 0:
+			$Sprite2D.scale.x = -1
 		else:
-			$Sprite2D["flip_h"] = false
+			$Sprite2D.scale.x = 1
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
+		state_machine.travel("idle")
+	
+	#handle attack
+	if Input.is_action_just_pressed("attack"):
+		attack()
 
 	move_and_slide()
+
+func attack():
+	state_machine.travel("attack")
+	if animation_tree.tree_root.get_node("attack").animation:
+		is_attacking = true
+	
+
+
+func _on_animation_tree_animation_finished(anim_name):
+	if anim_name == "attack":
+		is_attacking = false
