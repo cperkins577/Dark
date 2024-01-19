05bc5eb1507b5c38218d25915672efb55263d24e
Cole <cperkins577@gmail.com>
2024-01-18 22:39:16 -0500

    Added projectile animations

--- scenes/gun1/bullet.gd
+++ scenes/gun1/bullet.gd
@@ -1,9 +1,9 @@
 extends Area2D
 
+@export var speed = 500
 var travelled_distance = 0
 
 func _physics_process(delta):
-	var speed = 500
 	const RANGE = 200
 	var direction = Vector2.RIGHT.rotated(rotation)
 	position += direction * speed * delta
@@ -11,3 +11,12 @@ func _physics_process(delta):
 	travelled_distance += speed * delta
 	if travelled_distance > RANGE:
 		queue_free()
+
+func _on_body_entered(body):
+	speed = 0
+	$Projectile.scale.x = 1
+	$Projectile.scale.y = 1
+	$Projectile.play("Hit")
+	if body is Enemy:
+		body.take_damage()
+	
